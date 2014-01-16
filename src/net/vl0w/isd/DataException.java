724728bc229fbdce0873f9e32c7572adef4f2aeb
Jonas Hansen <jonas.hansen@bluewin.ch>
2014-01-16 21:34:42 +0100

    Deleting unused exception

--- src/net/vl0w/isd/DataException.java
+++
@@ -1,9 +0,0 @@
-package net.vl0w.isd;
-
-public class DataException extends Exception {
-	private static final long serialVersionUID = 353948646883671864L;
-
-	public DataException(String message) {
-		super(message);
-	}
-}
