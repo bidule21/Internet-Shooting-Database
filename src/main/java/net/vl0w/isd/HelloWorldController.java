95da7b3c4d122f58c60e13c2910893da016b59e2
Jonas Hansen <jonas.hansen@bluewin.ch>
2014-03-10 22:56:55 +0100

    Add HelloWorldController for testing Spring webmvc

--- src/main/java/net/vl0w/isd/HelloWorldController.java
+++ src/main/java/net/vl0w/isd/HelloWorldController.java
@@ -2,16 +2,14 @@ package net.vl0w.isd;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class EventController {
+public class HelloWorldController {
 
-	@RequestMapping("/event")
+	@RequestMapping("/hi")
 	@ResponseBody
-	public String event(
-			@RequestParam(value = "name", required = true) String name) {
-		return name;
+	public String helloWorld() {
+		return "Hello World";
 	}
 }
